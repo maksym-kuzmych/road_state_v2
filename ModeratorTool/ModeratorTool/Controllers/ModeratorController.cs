47a8fa1b253c338c2cbfcfb04d610bbac60b3c7f
Maxim9870 <kuzmichvania@mail.ru>
2020-05-17 23:08:00 +0300

    Added Moder Tool Project

---
+++ ModeratorTool/ModeratorTool/Controllers/ModeratorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace ModeratorTool.Controllers
+{
+    public class ModeratorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SendMail()
+        {
+            string recipient = Request["to"];
+            string subject = Request["subject"];
+            string body = Request["body"];
+
+            WebMail.SmtpServer = "smtp.gmail.com";
+            WebMail.SmtpPort = 587;
+            WebMail.SmtpUseDefaultCredentials = true;
+            WebMail.EnableSsl = true;
+            WebMail.UserName = "sigmaautoresponse@gmail.com";
+            WebMail.Password = "Qwerty322q";
+
+            WebMail.Send(recipient, subject, body, isBodyHtml: true);
+
+            return View();
+        }
+    }
+}
